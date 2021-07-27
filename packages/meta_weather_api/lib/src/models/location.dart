116147382b5499674d483e720c108f103afa0678
Rui Miguel Alonso <ruiskas@gmail.com>
2021-07-27 22:30:44 +0200

    New: json serialization for location model

--- packages/meta_weather_api/lib/src/models/location.dart
+++ packages/meta_weather_api/lib/src/models/location.dart
@@ -1,4 +1,41 @@
-enum LocationType { city, region, state, province, country, continent }
+import 'package:json_annotation/json_annotation.dart';
+
+part 'location.g.dart';
+
+enum LocationType {
+  @JsonValue('City')
+  city,
+  @JsonValue('Region')
+  region,
+  @JsonValue('State')
+  state,
+  @JsonValue('Province')
+  province,
+  @JsonValue('Country')
+  country,
+  @JsonValue('Continent')
+  continent,
+}
+
+@JsonSerializable()
+class Location {
+  Location({
+    required this.title,
+    required this.locationType,
+    required this.latLng,
+    required this.woeid,
+  });
+
+  factory Location.fromJson(Map<String, dynamic> json) =>
+      _$LocationFromJson(json);
+
+  final String title;
+  final LocationType locationType;
+  @JsonKey(name: 'latt_long')
+  @LatLongConverter()
+  final LatLng latLng;
+  final int woeid;
+}
 
 class LatLng {
   LatLng({
@@ -10,16 +47,20 @@ class LatLng {
   final double longitude;
 }
 
-class Location {
-  Location({
-    required this.title,
-    required this.locationType,
-    required this.latLng,
-    required this.woeid,
-  });
+class LatLongConverter implements JsonConverter<LatLng, String> {
+  const LatLongConverter();
 
-  final String title;
-  final LocationType locationType;
-  final LatLng latLng;
-  final int woeid;
+  @override
+  LatLng fromJson(String json) {
+    final parts = json.split(',');
+    return LatLng(
+      latitude: double.tryParse(parts[0]) ?? 0,
+      longitude: double.tryParse(parts[1]) ?? 0,
+    );
+  }
+
+  @override
+  String toJson(LatLng latlng) {
+    return '${latlng.latitude},${latlng.longitude}';
+  }
 }
