12f4f452f6e9e4968ebfa0a0081a355e9c64b4bf
Rui Miguel Alonso <ruiskas@gmail.com>
2021-08-28 20:20:26 +0200

    Fix: finished repository tests and coverage

--- packages/weather_repository/test/src/weather_repository_test.dart
+++ packages/weather_repository/test/src/weather_repository_test.dart
@@ -29,7 +29,125 @@ void main() {
     });
 
     group('getWeather', () {
-      test('instantiates ', () {});
+      const city = 'madrid';
+      const woeid = 766273;
+
+      test('calls locationSearch with a city', () async {
+        try {
+          await weatherRepository.getWeather(city);
+        } catch (_) {}
+        verify(() => metaWeatherApiClient.locationSearch(city)).called(1);
+      });
+
+      test('throws exception when locationSearch fails', () async {
+        final exception = Exception('oops');
+        when(() => metaWeatherApiClient.locationSearch(any()))
+            .thenThrow(exception);
+
+        expect(() => weatherRepository.getWeather(city), throwsA(exception));
+      });
+
+      test('calls getWeather with correct city', () async {
+        final location = MockLocation();
+        when(() => location.woeid).thenReturn(woeid);
+        when(() => metaWeatherApiClient.locationSearch(any()))
+            .thenAnswer((_) async => location);
+
+        try {
+          await weatherRepository.getWeather(city);
+        } catch (_) {}
+        verify(() => metaWeatherApiClient.getWeather(woeid)).called(1);
+      });
+
+      test('throws exception when getWeather fails', () async {
+        final exception = Exception('oops');
+        final location = MockLocation();
+        when(() => location.woeid).thenReturn(woeid);
+        when(() => metaWeatherApiClient.locationSearch(any()))
+            .thenAnswer((_) async => location);
+        when(() => metaWeatherApiClient.getWeather(any())).thenThrow(exception);
+
+        expect(() => weatherRepository.getWeather(city), throwsA(exception));
+      });
+
+      test('returns correct weatheron success (showers)', () async {
+        final location = MockLocation();
+        final weather = MockWeather();
+
+        when(() => location.woeid).thenReturn(woeid);
+        when(() => location.title).thenReturn('Madrid');
+        when(() => weather.weatherStateAbbr)
+            .thenReturn(meta_weather_api.WeatherState.showers);
+        when(() => weather.theTemp).thenReturn(40);
+
+        when(() => metaWeatherApiClient.locationSearch(any()))
+            .thenAnswer((_) async => location);
+        when(() => metaWeatherApiClient.getWeather(any()))
+            .thenAnswer((_) async => weather);
+
+        final actual = await weatherRepository.getWeather(city);
+        expect(
+          actual,
+          const Weather(
+            location: 'Madrid',
+            temperature: 40,
+            condition: WeatherCondition.rainy,
+          ),
+        );
+      });
+
+      test('returns correct weatheron success (heavyCloud)', () async {
+        final location = MockLocation();
+        final weather = MockWeather();
+
+        when(() => location.woeid).thenReturn(woeid);
+        when(() => location.title).thenReturn('Madrid');
+        when(() => weather.weatherStateAbbr)
+            .thenReturn(meta_weather_api.WeatherState.heavyCloud);
+        when(() => weather.theTemp).thenReturn(40);
+
+        when(() => metaWeatherApiClient.locationSearch(any()))
+            .thenAnswer((_) async => location);
+        when(() => metaWeatherApiClient.getWeather(any()))
+            .thenAnswer((_) async => weather);
+
+        final actual = await weatherRepository.getWeather(city);
+        expect(
+          actual,
+          const Weather(
+            location: 'Madrid',
+            temperature: 40,
+            condition: WeatherCondition.cloudy,
+          ),
+        );
+      });
+
+      //don't understand why coverage needs lightCloud and heavyCloud to cover 100%
+      test('returns correct weatheron success (lightCloud)', () async {
+        final location = MockLocation();
+        final weather = MockWeather();
+
+        when(() => location.woeid).thenReturn(woeid);
+        when(() => location.title).thenReturn('Madrid');
+        when(() => weather.weatherStateAbbr)
+            .thenReturn(meta_weather_api.WeatherState.lightCloud);
+        when(() => weather.theTemp).thenReturn(40);
+
+        when(() => metaWeatherApiClient.locationSearch(any()))
+            .thenAnswer((_) async => location);
+        when(() => metaWeatherApiClient.getWeather(any()))
+            .thenAnswer((_) async => weather);
+
+        final actual = await weatherRepository.getWeather(city);
+        expect(
+          actual,
+          const Weather(
+            location: 'Madrid',
+            temperature: 40,
+            condition: WeatherCondition.cloudy,
+          ),
+        );
+      });
     });
   });
 }
